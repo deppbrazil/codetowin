2c445ef74836a0e6edba940f43cf5440159e73df
Mir Carvalho <deppbrazil@gmail.com>
2019-11-26 18:38:56 -0300

    add base

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+
+function Dashboard() {
+    return (
+        <>
+            <header>Header</header>
+            <div>Dashboard</div>
+        </>
+    );
+}
+
+export default Dashboard;
---
+++ src/components/game/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+
+function Dashboard() {
+    return (
+        <>
+            <header>Header</header>
+            <div>Game</div>
+        </>
+    );
+}
+
+export default Dashboard;
