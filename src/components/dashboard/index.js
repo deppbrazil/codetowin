9cc162e9bd7dc288716a3e7af4f7fc9d3729a9b0
Mir Carvalho <deppbrazil@gmail.com>
2019-11-26 19:35:40 -0300

    add base header

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './style.css';
+import './text.css';
 
 
 function Dashboard() {
     return (
         <>
-            <header>Header</header>
-            <div>Dashboard</div>
+            <header className="head flexbox">
+                <div className="wrapper__head flexbox">
+                    <div className="wrapper__avatar flexbox">
+                        <div className="avatar"></div>
+                        <div className="wrapper__text">
+                            <h1 className="h1">user</h1>
+                            <h2 className="h2">ID:xxxxxx</h2>
+                        </div>
+                    </div>
+                    <div className="coin">
+                        <h1 className="h1">coin</h1>
+                    </div>
+                </div>
+                <div className="wrapper__head--center flexbox">
+                    <div className="coin">
+                        <h1 className="h1">flow</h1>
+                    </div>
+                </div>
+            </header>
+        
+            <section>
+                <div>Content</div>
+            </section>
         </>
     );
 }
